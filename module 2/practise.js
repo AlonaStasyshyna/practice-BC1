2be240fa5193d373d77d335d361644a79139ef23
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-08-10 18:23:15 +0300

    Add practice at module 2

---
+++ module 2/practise.js
@@ -0,0 +1,10 @@
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Change code below this line
+//   let totalPrice = message.split(" ").length * pricePerWord;
+
+//   console.log(totalPrice);
+
+//   // Change code above this line
+// }
+
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
