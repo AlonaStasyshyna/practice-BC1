e7b8171674398c4142cfca44d218d5a49ef1e494
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-08-12 01:03:00 +0300

    Add practice at module 2

--- module 2/practise.js
+++ module 2/practise.js
@@ -8,3 +8,65 @@
 // }
 
 // console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+
+// function calculateTotal(number) {
+//   // Change code below this line
+//   let summa = 0;
+
+//   for (let i = 1; i <= number; i += 1) {
+//     summa += i;
+//   }
+//   return summa;
+
+//   // Change code above this line
+// }
+
+// calculateTotal(3);
+// calculateTotal(7);
+
+// function findLongestWord(string) {
+//   // Change code below this line
+//   const stringToArr = string.split(" ");
+//   let longestWord = stringToArr[0];
+
+//   for (let i = 0; i < stringToArr.length; i += 1) {
+//     if (stringToArr[i].length > longestWord.length) {
+//       longestWord = stringToArr[i];
+//     }
+//   }
+
+//   return longestWord;
+
+//   // Change code above this line
+// }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+
+//   for (let i = min; i <= max; i += 1) {
+//     numbers.push(i);
+//   }
+//   // Change code above this line
+//   console.log(numbers);
+//   return numbers;
+// }
+
+// console.log(createArrayOfNumbers(14, 17));
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const numbersNew = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     console.log(numbers[i]);
+//     if (numbers[i] > value) {
+//       numbersNew.push(numbers[i]);
+//     }
+//   }
+//   return numbersNew;
+//   // Change code above this line
+// }
+
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
