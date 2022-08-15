12c93b43c4907270f73ff5edcf48478ccf076d92
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-08-15 18:55:29 +0300

    Add practice 3

--- module 2/practice.js
+++ module 2/practice.js
@@ -156,3 +156,85 @@
 // }
 
 // console.log(countProps({ name: "Mango", age: 2 }));
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   //   console.log(color);
+//   hexColors.push(color["hex"]);
+//   rgbColors.push(color["rgb"]);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+//   // Change code above this line
+// }
+
+// console.log(getProductPrice("Scanner"));
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const searchArr = [];
+
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       searchArr.push(product[propName]);
+//     }
+//   }
+//   return searchArr;
+//   // Change code above this line
+// }
+
+// console.log(getAllPropValues("name"));
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Change code below this line
+  for (const product of products) {
+    if (productName === product.name) {
+      return product.price * product.quantity;
+    }
+  }
+  return 0;
+  // Change code above this line
+}
+
+console.log(calculateTotalPrice("Radar"));
+console.log(calculateTotalPrice("Blaster"));
